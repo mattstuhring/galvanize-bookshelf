32ca93d10badbba5e6f2864be20d18432b60e41f
Matt Stuhring <whereyouatmatt@gmail.com>
2016-06-29 14:58:04 -0700

    Add user registration

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,29 @@
 
 const express = require('express');
 const router = express.Router();
+const knex = require('../knex');
+const bcrypt = require('bcrypt');
 
-// YOUR CODE HERE
+router.post('/users', (req, res, next) => {
+  bcrypt.hash(req.body.password, 12, (hashErr, hashed_password) => {
+    if (hashErr) {
+      return next(hashErr);
+    }
+
+    knex('users')
+      .insert({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        hashed_password: hashed_password
+      })
+      .then((users) => {
+        res.sendStatus(200);
+      })
+      .catch((hashErr) => {
+        next(hashErr);
+      });
+  });
+});
 
 module.exports = router;
