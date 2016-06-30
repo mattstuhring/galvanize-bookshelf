befd5f61e813af184817fa286b5cd8f1dac6f952
Matt Stuhring <whereyouatmatt@gmail.com>
2016-06-30 16:26:20 -0700

    commit green check

--- routes/users_books.js
+++ routes/users_books.js
@@ -3,6 +3,46 @@
 const express = require('express');
 const router = express.Router();
 
-// YOUR CODE HERE
+const knex = require('../knex');
+const bcrypt = require('bcrypt');
+
+const checkAuth = function(req, res, next) {
+  if (!req.session.user) {
+    return res.sendStatus(401);
+  }
+
+  next();
+};
+
+router.get('/user/books', checkAuth, (req, res, next) => {
+  const userId = req.session.user.id;
+
+  knex('books')
+    .innerJoin('users_books', 'users_books.book_id', 'books.id')
+    .where('users_books.user_id', userId)
+    .then((books) => {
+      res.send(books);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.post('/users/books/:bookId', checkAuth, (req, res, next) => {
+  const userId = req.session.user.id;
+  const bookId = Number.parseInt(req.params.bookId);
+
+  knex('users_books')
+    .insert({
+      user_id: userId,
+      book_id: bookId
+    }, '*')
+    .then((results) => {
+      res.send(results[0]);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
