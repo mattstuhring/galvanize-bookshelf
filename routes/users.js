862f17bcac319cd73b9778c179629e5d54db5b89
Matt Stuhring <whereyouatmatt@gmail.com>
2016-06-30 14:12:49 -0700

    Add validations

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,23 @@ const knex = require('../knex');
 const bcrypt = require('bcrypt');
 
 router.post('/users', (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (!email || email.trim() === '') {
+    return res
+      .status(400)
+      .set('Content-type', 'text/plain')
+      .send('Email must not be blank');
+  }
+
+  if (!password || password.trim() === '') {
+    return res
+      .status(400)
+      .set('Content-type', 'text/plain')
+      .send('Password must not be blank');
+  }
+
   bcrypt.hash(req.body.password, 12, (hashErr, hashed_password) => {
     if (hashErr) {
       return next(hashErr);
