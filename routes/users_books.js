c032805ee6947936bc27281527f63bc88668a06d
Matt Stuhring <whereyouatmatt@gmail.com>
2016-07-01 15:56:30 -0700

    commit

--- routes/users_books.js
+++ routes/users_books.js
@@ -2,20 +2,18 @@
 
 const express = require('express');
 const router = express.Router();
-
 const knex = require('../knex');
-const bcrypt = require('bcrypt');
 
 const checkAuth = function(req, res, next) {
-  if (!req.session.user) {
+  if (!req.session.userId) {
     return res.sendStatus(401);
   }
 
   next();
 };
 
-router.get('/user/books', checkAuth, (req, res, next) => {
-  const userId = req.session.user.id;
+router.get('/users/books', checkAuth, (req, res, next) => {
+  const userId = req.session.userId;
 
   knex('books')
     .innerJoin('users_books', 'users_books.book_id', 'books.id')
@@ -28,17 +26,81 @@ router.get('/user/books', checkAuth, (req, res, next) => {
     });
 });
 
-router.post('/users/books/:bookId', checkAuth, (req, res, next) => {
-  const userId = req.session.user.id;
+router.get('/users/books/:bookId', checkAuth, (req, res, next) => {
+  const userId = req.session.userId;
   const bookId = Number.parseInt(req.params.bookId);
 
+  knex('books')
+    .innerJoin('users_books', 'users_books.book_id', 'books.id')
+    .where('users_books.user_id', userId)
+    .where('users_books.book_id', bookId)
+    .first()
+    .then((book) => {
+      if (!book) {
+        return next();
+      }
+      res.send(book);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.post('/users/books/:bookId', checkAuth, (req, res, next) => {
+  const userId = req.session.userId;
+  const bookId = Number.parseInt(req.params.bookId);
+
+  if (Number.isNaN(bookId)) {
+    return next();
+  }
+
+  knex('books')
+    .where('id', bookId)
+    .first()
+    .then((book) => {
+      if (!book) {
+        return next();
+      }
+
+      return knex('users_books')
+        .insert({
+          user_id: userId,
+          book_id: bookId
+        }, '*')
+        .then((results) => {
+          res.send(results[0]);
+        });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.delete('/users/books/:bookId', checkAuth, (req, res, next) => {
+  const userId = req.session.userId;
+  const bookId = Number.parseInt(req.params.bookId);
+
+  if (Number.isNaN(bookId)) {
+    return next();
+  }
+
   knex('users_books')
-    .insert({
-      user_id: userId,
-      book_id: bookId
-    }, '*')
-    .then((results) => {
-      res.send(results[0]);
+    .where('user_id', userId)
+    .where('book_id', bookId)
+    .first()
+    .then((user_book) => {
+      if (!user_book) {
+        return next();
+      }
+
+      return knex('users_books')
+        .where('user_id', userId)
+        .where('book_id', bookId)
+        .del()
+        .then(() => {
+          delete user_book.id;
+          res.send(user_book);
+        });
     })
     .catch((err) => {
       next(err);
